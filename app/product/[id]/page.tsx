b8bda40156de6f0a66de4a8bd73a7784de4c92c8
pobre <nacholapto@gmail.com>
2025-06-12 20:00:21 +0200

    fix: async function [id] app/page

--- app/product/[id]/page.tsx
+++ app/product/[id]/page.tsx
@@ -1,11 +1,9 @@
 import { ProductDetailPage } from '@/screens';
 import React from 'react'
 
-const ProductDetail = async ({ params }: { params: Promise<{ id: string }> }) => {
+export default function ProductDetail({ params }: { params: Promise<{ id: string }> }){
     const resolvedParams = React.use(params)
     return (
         <ProductDetailPage id={resolvedParams.id} />
     );
 }
-
-export default ProductDetail;
